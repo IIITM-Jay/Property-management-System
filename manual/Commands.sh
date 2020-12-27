a1bde20716cdfa2f7c7992122d0bb7b86adbb666
Jay Dev Jha <65283880+IIITM-Jay@users.noreply.github.com>
2020-12-27 16:25:48 +0530

    Create Commands.sh

---
+++ manual/Commands.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python-setuptools
+sudo easy_install virtualenv
+
+# or 
+
+sudo apt install virtualenv 
+sudo apt install python3-django
+django-admin --version
+source venv/bin/activate
