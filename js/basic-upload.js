fe7b99f58da95ebebd41d6f17fc48f06f8054276
Jay Dev Jha <65283880+IIITM-Jay@users.noreply.github.com>
2020-12-27 12:44:55 +0530

    Add files via upload

---
+++ js/basic-upload.js
@@ -0,0 +1,32 @@
+
+$(function (){
+
+  $(".js-upload-photos").click(function (){
+    $("#fileupload").click();
+  });
+
+  $("#fileupload").fileupload({
+    dataType:'json',
+    sequentialUploads: true,
+    start: function(e) {
+      $("#modal-progress").modal("show");
+    },
+    stop: function(e) {
+      $("#modal-progress").modal("hide");
+    },
+    progressall: function (e,data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      var strProgress = progress + "%";
+      $(".progress-bar").css({"width": strProgress});
+      $(".progress-bar").text(strProgress);
+    },
+    done: function (e,data) {
+      if (data.result.is_valid) {
+        console.log("I ran")
+        $("#gallery tbody").prepend(
+          "<tr><td><a href='" + data.result.url +"'>" + data.result.name + "<a/></td></tr>"
+        )
+      }
+    }
+  });
+});
